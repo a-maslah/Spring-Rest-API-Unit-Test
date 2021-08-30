63af0c8c636870ab5f0873c6acd987c7f8f9393a
a-maslah <maslahamine20@gmail.com>
2021-08-30 16:16:52 +0100

    ngrok test

--- src/main/java/com/sundaydevblog/springrestapitest/controller/MemberController.java
+++ src/main/java/com/sundaydevblog/springrestapitest/controller/MemberController.java
@@ -72,7 +72,7 @@ public class MemberController {
 
     @GetMapping(value = "/hello")
     public String sayHi(){
-        return "Say hello to my little friend,Jenkins Pipeline CICD!";
+        return "Say hello to my little friend,JenkinsTset Pipeline CICD!";
     }
      @GetMapping(value = "/test")
     public String sayHey(){
